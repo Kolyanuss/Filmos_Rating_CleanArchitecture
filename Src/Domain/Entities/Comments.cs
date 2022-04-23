f6be6ebec53a2d68d276eb1e9004925666242e58
0xKolyanus <mukola.1zt.@gmail.com>
2022-04-23 15:49:01 +0300

    modify comment entity

--- Src/Domain/Entities/Comments.cs
+++ Src/Domain/Entities/Comments.cs
@@ -1,10 +1,18 @@
-﻿namespace Filmos_Rating_CleanArchitecture.Domain.Entities
+﻿using System.Collections.Generic;
+
+namespace Filmos_Rating_CleanArchitecture.Domain.Entities
 {
     public class Comments
     {
+        public Comments()
+        {
+            Films = new HashSet<Films>();
+            Users = new HashSet<Users>();
+        }
+
         public int Id_comment { get; set; }
         public int Text { get; set; }
-        public int Id_film { get; set; }
-        public int Id_user { get; set; }
+        public ICollection<Films> Films { get; private set; }
+        public ICollection<Users> Users { get; private set; }
     }
 }
