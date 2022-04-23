e8f7bf53e45cc3974045fea5ff09464164c89076
0xKolyanus <mukola.1zt.@gmail.com>
2022-04-23 15:24:32 +0300

    start create a project

---
+++ Domain/Entities/Films.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmos_Rating_CleanArchitecture.Domain.Entities
+{
+    public class Films
+    {
+        public int id_film { get; set; }
+        public int name_film { get; set; }
+    }
+}
