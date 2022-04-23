1a9006c83589fd162e108a349223e667c59da2f7
0xKolyanus <mukola.1zt.@gmail.com>
2022-04-23 16:07:17 +0300

    add Application layer

---
+++ Src/Application/Films/Commands/DeleteFilms/DeleteFilmsCommand.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filmos_Rating_CleanArchitecture.Application.Films.Commands.DeleteFilms
+{
+    public class DeleteFilmsCommand : IRequest
+    {
+        public int Id { get; set; }
+    }
+}
